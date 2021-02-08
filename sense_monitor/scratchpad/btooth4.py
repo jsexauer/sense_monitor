379e4863afe77d54a28aa0725fff5bcfa9bc4366
Jason Sexauer <GenericCarbonLifeform@gmail.com>
2021-02-08 12:09:27 -0500

    Update btooth4.py
    A different way to scan for the phone

--- sense_monitor/scratchpad/btooth4.py
+++ sense_monitor/scratchpad/btooth4.py
@@ -1,7 +1,8 @@
 # This one works!
 # Phone = 18:4e:16:94:38:af
-
+import datetime
 from bluepy.btle import Scanner, DefaultDelegate
+from time import sleep
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -13,10 +14,32 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:pass
             #print("Received new data from", dev.addr)
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(2.0)
 
-for dev in devices:
-    print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        print("  %s = %s" % (desc, value))
+
+def print_devices():
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(2.0)
+
+    for dev in devices:
+        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            print("  %s = %s" % (desc, value))
+            
+def scan_for_phone():
+    while True:
+        scanner = Scanner().withDelegate(ScanDelegate())
+        devices = scanner.scan(2.0)
+        
+        print('*'*20)
+        print(datetime.datetime.now())
+        for dev in devices:
+            found_services = 0
+            for (adtype, desc, value) in dev.getScanData():
+                if value in ('0000fd6f-0000-1000-8000-00805f9b34fb', '6ffd96b27d212f47562ffd7b037fb048baf1e57d27df'):
+                    found_services += 1
+            if found_services == 2:
+                print(f"Found phone {dev.addr} at {dev.rssi}")
+         sleep(15)
+        
+if __name__=='__main__':
+    scan_for_phone()
