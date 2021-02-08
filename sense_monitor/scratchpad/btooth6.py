9825d7b3e5f95fa56d3599a6ddd8e9f2969f7632
Jason Sexauer <GenericCarbonLifeform@gmail.com>
2021-02-08 16:39:24 -0500

    attempt fix

--- sense_monitor/scratchpad/btooth6.py
+++ sense_monitor/scratchpad/btooth6.py
@@ -24,7 +24,7 @@ class BluetoothRSSI(object):
     def prep_cmd_pkt(self):
         """Prepares the command packet for requesting RSSI"""
         reqstr = struct.pack(
-            "6sB17s", bt.str2ba(self.addr), bt.ACL_LINK, "\0" * 17)
+            "6sB17s", bt.str2ba(self.addr), bt.ACL_LINK, b"\0" * 17)
         request = array.array("c", reqstr)
         handle = fcntl.ioctl(self.hci_fd, bt.HCIGETCONNINFO, request, 1)
         handle = struct.unpack("8xH14x", request.tostring())[0]
