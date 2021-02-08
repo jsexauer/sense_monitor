c2e4fc37bd9c6b7ca8f2f5e0f6a10180b13c7e27
Jason Sexauer <GenericCarbonLifeform@gmail.com>
2021-02-08 12:15:57 -0500

    Update btooth4.py
    refine

--- sense_monitor/scratchpad/btooth4.py
+++ sense_monitor/scratchpad/btooth4.py
@@ -33,12 +33,14 @@ def scan_for_phone():
         print('*'*20)
         print(datetime.datetime.now())
         for dev in devices:
-            found_services = 0
+            found_services = False
+            dev_desc = "Device %s (%s), RSSI=%d dB\n" % (dev.addr, dev.addrType, dev.rssi)
             for (adtype, desc, value) in dev.getScanData():
-                if value in ('0000fd6f-0000-1000-8000-00805f9b34fb', '6ffd96b27d212f47562ffd7b037fb048baf1e57d27df'):
-                    found_services += 1
-            if found_services == 2:
-                print(f"Found phone {dev.addr} at {dev.rssi}")
+                dev_desc += "  %s = %s [%s]\n" % (desc, value, adtype))
+                if value in ('0000fd6f-0000-1000-8000-00805f9b34fb'):
+                    found_services = True                    
+            if found_services:
+                print(dev_desc)
         sleep(15)
         
 if __name__=='__main__':
