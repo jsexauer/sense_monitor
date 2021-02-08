436ea8f865583b6002f9eaef473e2b5c5fd53ab6
Jason Sexauer <GenericCarbonLifeform@gmail.com>
2021-02-08 16:41:07 -0500

    attempt fix 3

--- sense_monitor/scratchpad/btooth6.py
+++ sense_monitor/scratchpad/btooth6.py
@@ -50,7 +50,7 @@ class BluetoothRSSI(object):
             rssi = bt.hci_send_req(
                 self.hci_sock, bt.OGF_STATUS_PARAM,
                 bt.OCF_READ_RSSI, bt.EVT_CMD_COMPLETE, 4, self.cmd_pkt)
-            rssi = struct.unpack('b', rssi[3])[0]
+            rssi = struct.unpack('b', bytes(rssi)[3])[0]
             return rssi
         except IOError:
             # Happens if connection fails (e.g. device is not in range)
